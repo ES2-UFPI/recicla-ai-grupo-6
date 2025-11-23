5a4930d6a6b81f09e1695285ada5ec2331779f10
Rafael <franciscorafaell789@gmail.com>
2025-11-23 00:01:39 -0300

    RED: implementa teste para a issue #82

--- backend/src/aplicativo_web/tests.py
+++ backend/src/aplicativo_web/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class CooperativaNotificationTest(TestCase):
+    """Teste TDD: quando uma nova cooperativa se cadastrar, uma notificação
+    (push / popup no navegador) deve ser disparada para coletores ativos/logados.
+    """
+
+    def test_notification_sent_to_active_collectors_on_cooperativa_create(self):
+        # TODO: implementar cenário de criação de cooperativa e verificação
+        # de que notificações foram enviadas aos coletores ativos.
+        # Por enquanto: marcar como não-implementado para que o teste falhe
+        # obedecendo a prática TDD inicial.
+        self.fail('Not implemented: push notification dispatch for new cooperativa')
