efd98f9244affa2076b8a1b86beb5edee09e77de
Rafael V <rafaelvazrocha35@gmail.com>
2025-11-25 01:39:18 -0300

    refatoracao de codigo e integracao de back e front para a issue #58

--- backend/src/aplicativo_web/urls.py
+++ backend/src/aplicativo_web/urls.py
@@ -16,7 +16,9 @@ from .views import (
     # escolha_cooperativa_view
     AvaliarProdutorView,
     ProdutorPerfilView,
-    AvaliarColetorView
+    AvaliarColetorView,
+    EntregasPendentesCooperativaView,
+    ColetorPerfilView,
 )
 
 urlpatterns = [
@@ -51,6 +53,9 @@ urlpatterns = [
     # path('list_cooperativas/', views.escolha_cooperativa_view, name='list-cooperativas'),
     path('avaliar/produtor/', AvaliarProdutorView.as_view(), name='avaliar-produtor'),
     path('produtor/perfil/', ProdutorPerfilView.as_view(), name='produtor-perfil'),
+    path('coletor/perfil/', ColetorPerfilView.as_view(), name='coletor-perfil'),
     path("avaliar/produtor/", AvaliarProdutorView.as_view(), name="avaliar-produtor"),
     path("avaliar/coletor/", AvaliarColetorView.as_view(), name="avaliar-coletor"),
+    path('coletas/pendentes_cooperativa/', EntregasPendentesCooperativaView.as_view(),
+         name='coletas-pendentes-cooperativa'),
 ]
